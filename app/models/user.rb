ff4b92029192fb3453e1d6a2459cfacbfc649e35
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-25 15:02:37 +0530

    Setup User using devise gem

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
