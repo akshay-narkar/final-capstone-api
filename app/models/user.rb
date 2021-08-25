f663476c4eaf1ce39ac26a4cc70d5dd7107d606a
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-25 19:23:24 +0530

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   extend Devise::Models
+  has_many :favs
+  has_many :courses, :through => :favs 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
