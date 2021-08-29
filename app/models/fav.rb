396b71984b3670c9675eb609fe7bd2eddb11015b
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-29 19:50:11 +0530

    Remove linter errors

--- app/models/fav.rb
+++ app/models/fav.rb
@@ -2,5 +2,5 @@ class Fav < ApplicationRecord
   belongs_to :user
   belongs_to :course
   validates :user_id, :course_id, presence: true, numericality: { only_integer: true }
-  validates_uniqueness_of :user_id, { scope: :course_id, message: "Course already liked!" }
+  validates_uniqueness_of :user_id, { scope: :course_id, message: 'Course already liked!' }
 end
