f1f6d394a6465f2b44da1d465550169b3c63d12f
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-29 19:44:28 +0530

    Update device auth file

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -5,7 +5,7 @@ DeviseTokenAuth.setup do |config|
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
   # each request.
-  # config.change_headers_on_each_request = true
+  # config.change_headers_on_each_request = false
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
@@ -17,6 +17,7 @@ DeviseTokenAuth.setup do |config|
   config.token_cost = Rails.env.test? ? 4 : 10
   config.default_confirm_success_url = true
   config.default_confirm_success_url
+  config.change_headers_on_each_request = false
   # Sets the max number of concurrent devices per user, which is 10 by default.
   # After this limit is reached, the oldest tokens will be removed.
   # config.max_number_of_devices = 10
