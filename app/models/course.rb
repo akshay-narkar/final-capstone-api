095576144b0f8cb8ea8656fe33d1d2a8bfe6536d
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-29 19:45:00 +0530

    Update course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
-    has_many :favs
-    has_many :users, :through => :favs
+  has_many :favs, foreign_key: :course_id, dependent: :destroy
+  has_many :users, :through => :favs
+  validates :name, :domain, :teacher_name, presence: true
+  validates :fee, presence: true
+  validates :rating, presence: true, numericality: {greater_than: -1, less_than: 6}
 end
