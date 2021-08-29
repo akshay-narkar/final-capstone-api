a5461395f8212f1cc02a1e7eae7c97ebb1024d14
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-29 19:45:23 +0530

    Update fav model

--- app/models/fav.rb
+++ app/models/fav.rb
@@ -1,5 +1,6 @@
 class Fav < ApplicationRecord
-    belongs_to :user
-    belongs_to :course
-    validates_uniqueness_of :user_id, scope: %i[course_id]
+  belongs_to :user
+  belongs_to :course
+  validates :user_id, :course_id, presence: true, numericality: { only_integer: true }
+  validates_uniqueness_of :user_id, { scope: :course_id, message: "Course already liked!" }
 end
