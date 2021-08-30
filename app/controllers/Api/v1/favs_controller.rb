5b8303240c007bdc52dcb0b4fef28b5a46649337
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-30 22:08:57 +0530

    Remove linter errors and create private function to pass params

--- app/controllers/Api/v1/favs_controller.rb
+++ app/controllers/Api/v1/favs_controller.rb
@@ -5,34 +5,38 @@ module Api
 
       def index
         favs = current_user.courses
-        # favs = User.find_by_id(newparams[:user_id]).courses;
+        # favs = User.find_by_id(newparams[:user_id]).courses
         render json: { status: 'SUCCESS', message: 'Courses Found', data: favs }, status: :ok
       end
 
       def create
-        # fav = Fav.create(user_id: current_user.id,course_id: newparams[:id])
-        fav = current_user.favs.build(newparams)
+        # fav = Fav.create(user_id: newparams[:user_id], course_id: courseparams[:course_id])
+        fav = current_user.favs.build(courseparams)
         if fav.save
-          render json: { status: 'SUCCESS', message: 'Add to favorites', data: fav }, status: :ok
+          render json: { status: 'SUCCESS', message: 'Added to favorites', data: fav }, status: :ok
         else
-          render json: { status: 'Failure', message: 'Could not add to favorites', data: fav.errors }, status: :fail
+          render json: { status: 'Failure', message: 'Could not add to favorites', data: fav.errors }, status: status
         end
       end
 
       def destroy
-        favs = current_user.favs.where(course_id: newparams[:course_id])
-        # delete = Fav.where(user_id: current_user.id,course_id: newparams[:id])
+        delete = current_user.favs.where(course_id: newparams[:course_id])
+        # delete = Fav.where(user_id: newparams[:user_id], course_id: courseparams[:course_id])
         if favs.destroy
           render json: { status: 'SUCCESS', message: 'Removed from favorites', data: delete }, status: :ok
         else
-          render json: { status: 'Failure', message: 'Could not remove from favorites', data: delete.errors }, status: :fail
+          render json: { status: 'Failure', message: 'Could not remove from favorites', data: delete.errors }, status: status
         end
       end
 
       private
 
       def newparams
-        params.permit(:user_id, :id, :course_id)
+        params.permit(:user_id)
+      end
+
+      def courseparams
+        params.permit(:course_id)
       end
     end
   end
