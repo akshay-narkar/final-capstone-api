9d68fd9459801e655a0f9bc2289e0d7b449bda1b
akshay-narkar <akshaynarkar1295@gmail.com>
2021-08-31 14:46:56 +0530

    Use current_user helper from devise

--- app/controllers/Api/v1/favs_controller.rb
+++ app/controllers/Api/v1/favs_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class FavsController < ApplicationController
-      before_action :authenticate_user!
+      before_action :authenticate_api_v1_user!
 
       def index
         favs = current_user.courses
