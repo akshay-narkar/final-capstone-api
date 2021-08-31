eec0f4da02595e938df6221ee6238a57c492c6a3
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-01 01:53:12 +0530

    Add conditional statements to the destroy route

--- app/controllers/Api/v1/favs_controller.rb
+++ app/controllers/Api/v1/favs_controller.rb
@@ -4,14 +4,14 @@ module Api
       before_action :authenticate_api_v1_user!
 
       def index
-        favs = current_user.courses
+        favs = current_api_v1_user.courses
         # favs = User.find_by_id(newparams[:user_id]).courses
         render json: { status: 'SUCCESS', message: 'Courses Found', data: favs }, status: :ok
       end
 
       def create
         # fav = Fav.create(user_id: newparams[:user_id], course_id: courseparams[:course_id])
-        fav = current_user.favs.build(courseparams)
+        fav = current_api_v1_user.favs.build(courseparams)
         if fav.save
           render json: { status: 'SUCCESS', message: 'Added to favorites', data: fav }, status: :ok
         else
@@ -20,12 +20,15 @@ module Api
       end
 
       def destroy
-        delete = current_user.favs.where(course_id: newparams[:course_id])
-        # delete = Fav.where(user_id: newparams[:user_id], course_id: courseparams[:course_id])
-        if favs.destroy
-          render json: { status: 'SUCCESS', message: 'Removed from favorites', data: delete }, status: :ok
+        delete1 = current_api_v1_user.favs.find_by(course_id: params[:course_id])
+        if delete1
+          if delete1.destroy
+            render json: { status: 'SUCCESS', message: 'Removed from favorites', data: delete1 }, status: :ok
+          else
+            render json: { status: 'Failure', message: 'Could not remove from favorites', data: delete1.errors }, status: status
+          end
         else
-          render json: { status: 'Failure', message: 'Could not remove from favorites', data: delete.errors }, status: status
+          render json: { status: 'Failure', message: 'No data found', data: delete1 }, status: status
         end
       end
 
