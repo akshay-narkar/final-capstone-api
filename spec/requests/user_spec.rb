04e417cec08d624cc749e23c22e2e0719b159128
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-01 01:52:09 +0530

    Remove linter errors

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'User', type: :request do
   let!(:users) { create(:user) }
 
   let(:valid_params1) { { email: 'abc155120@example.com', password: 'foobar', password_confirmation: 'foobar' } }
-  let(:valid_params2) { { email: 'foo@bar.com', password: 'foobar'} }
+  let(:valid_params2) { { email: 'foo@bar.com', password: 'foobar' } }
 
   describe 'Signup' do
     # make HTTP get request before each example
@@ -31,7 +31,6 @@ RSpec.describe 'User', type: :request do
     it 'returns courses' do
       # Note `json` is a custom helper to parse JSON responses
       expect(json['data']['email']).to eq('foo@bar.com')
-     
     end
 
     it 'returns status code 200' do
@@ -45,10 +44,9 @@ RSpec.describe 'User', type: :request do
 
     it 'returns courses' do
       auth_params = get_auth_params_from_login_response_headers(response)
-      delete '/api/v1/auth/sign_out', params: auth_params 
-      expect(json["success"]).to be_truthy
+      delete '/api/v1/auth/sign_out', params: auth_params
+      expect(json['success']).to be_truthy
       expect(response.status).to eq(200)
-   
     end
   end
 end
@@ -57,19 +55,18 @@ def login
   post '/api/v1/auth/sign_in', params: valid_params2
 end
 
- def get_auth_params_from_login_response_headers(response)
-    client = response.headers['client']
-    token = response.headers['access-token']
-    expiry = response.headers['expiry']
-    token_type = response.headers['token-type']
-    uid = response.headers['uid']
+def get_auth_params_from_login_response_headers(response)
+  client = response.headers['client']
+  token = response.headers['access-token']
+  expiry = response.headers['expiry']
+  token_type = response.headers['token-type']
+  uid = response.headers['uid']
 
-    auth_params = {
-      'access-token' => token,
-      'client' => client,
-      'uid' => uid,
-      'expiry' => expiry,
-      'token-type' => token_type
-    }
-    auth_params
-  end
+  {
+    'access-token' => token,
+    'client' => client,
+    'uid' => uid,
+    'expiry' => expiry,
+    'token-type' => token_type
+  }
+end
