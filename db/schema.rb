718b67f88df4a6357f19b3e42d5e993cf5689d07
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-01 15:26:09 +0530

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_081346) do
+ActiveRecord::Schema.define(version: 2021_09_01_095217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_081346) do
     t.string "course_photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "details"
   end
 
   create_table "favs", force: :cascade do |t|
